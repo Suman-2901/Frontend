38451b471d2d3d1017f0be22f0b44201bfa39c32
Suman-2901 <suma230102060@iiitmanipur.ac.in>
2024-08-27 22:24:19 +0530

    Listing page

--- New_product.js
+++ New_product.js
@@ -24,10 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
             categorySelect.appendChild(option);
         });
         products=data;
-
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
+        
     });
 
 
